0b521f7473bd1c2f4b02b1dceeb6d591d2f2106b
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2026-01-05 11:16:10 +0530

    yaap

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/blaze_emu64x.mk \
-
-	COMMON_LUNCH_CHOICES := \
-		blaze_emu64x-userdebug \
-		blaze_emu64x-user \
-		blaze_emu64x-eng
+	$(LOCAL_DIR)/yaap_emu64x.mk
