5363b9f0b89dd2819bc85b1339c7bf43672c82e6
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2026-01-05 11:18:20 +0530

    bringup yaap

--- yaap_emu64x.mk
+++ yaap_emu64x.mk
@@ -1,7 +1,7 @@
-$(call inherit-product, device/fus0g/emu64x/Board.mk)
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, device/generic/emu64x/Board.mk)
+$(call inherit-product, vendor/yaap/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := blaze_emu64x
+PRODUCT_NAME := yaap_emu64x
 PRODUCT_MODEL := ProjectBlaze Android SDK built for x86_64
 
 PRODUCT_SDK_ADDON_COPY_FILES += \
